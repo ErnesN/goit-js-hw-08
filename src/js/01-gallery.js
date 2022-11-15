b524d8efd213f6d3f980d39f58a41d6090bcd2db
Ernes Nafieiev <demodekoz@gmail.com>
2022-11-15 15:53:40 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,39 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const createGalleryItems = ({ preview, original, description }) => 
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+
+const makeGalleryItem = galleryItems.map(createGalleryItems).join('');
+galleryEl.insertAdjacentHTML('beforeend', makeGalleryItem);
+
+
+const onGalleryImageClick = e => {
+  e.preventDefault();
+  if (e.code === 'Escape') {
+    modal.close();
+  }
+
+  const createMarkupModalElementsGallery = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="auto" height="auto">`);
+  createMarkupModalElementsGallery.show();
+
+  document.addEventListener('keydown', e => {
+    if (e.code === 'Escape') {
+      createMarkupModalElementsGallery.close();
+    }
+  });
+};
+
+galleryEl.addEventListener('click', onGalleryImageClick);
