36bb3500c8474fcd43157f41ef22f7a7b28324d8
Ernes Nafieiev <demodekoz@gmail.com>
2022-11-18 23:52:13 +0200

    add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,6 @@
-
+import throttle from "lodash.throttle";
+import '../css/common.css';
+import '../css/03-feedback.css';
 // 1. Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message,
 // у яких зберігай поточні значення полів форми.Нехай ключем для сховища буде рядок "feedback-form-state".
 // 2. Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми.
@@ -11,23 +13,35 @@ const refs = {
     form: document.querySelector('.feedback-form'),
     inputEmail: document.querySelector('input'),
     inputMessage: document.querySelector('textarea'),
-    button: document.querySelector('buttom'),
+    button: document.querySelector('button'),
 }
+const STORAGE_KEY_MESSAGE = 'feedback-form-state';
+const formData = {};
 
 refs.form.addEventListener('submit', onSubmitForm);
-refs.inputMessage.addEventListener('input', onTextareaImput);
-refs.inputEmail.addEventListener('input', onEmailInput);
+refs.inputMessage.addEventListener('input', throttle(onTextareaImput, 2000));
+refs.form.addEventListener('input', e => {
+    formData[e.target.name] = e.target.value;
+});
 
+populateMessageOutput();
+function onSubmitForm(evt) {
+    evt.preventDefault();
 
-function onSubmitForm(evt) {}
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY_MESSAGE);
+}
 
 
 function onTextareaImput(evt) { 
-    const message = evt.currentTarget.value;
-    localStorage.setItem('feedback-form-state', message);
+    const message = evt.target.value;
+    localStorage.setItem(STORAGE_KEY_MESSAGE, message);
 }
 
 
-function onEmailInput(evt) { }
-
-function populateMessageOutput() {}
+function populateMessageOutput() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY_MESSAGE);
+    if (savedMessage) {
+     refs.inputMessage.value = savedMessage;   
+    }
+}
