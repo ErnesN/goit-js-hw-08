c02b001f002688284c35a46e498d5ec78f3c022f
Ernes Nafieiev <demodekoz@gmail.com>
2022-11-20 02:19:05 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -13,16 +13,70 @@ const refs = {
     form: document.querySelector('.feedback-form'),
     inputEmail: document.querySelector('input'),
     inputMessage: document.querySelector('textarea'),
-    button: document.querySelector('button'),
 }
 const STORAGE_KEY_MESSAGE = 'feedback-form-state';
-const formData = {};
+const STORAGE_KEY_EMAIL = 'formData';
 
 refs.form.addEventListener('submit', onSubmitForm);
-refs.inputMessage.addEventListener('input', throttle(onTextareaImput, 2000));
-refs.form.addEventListener('input', e => {
+refs.inputMessage.addEventListener('input', throttle(onTextareaImput, 500));
+refs.form.addEventListener('input', getFormDataToLS);
+const formData = {};
+const getFormDataToLS = STORAGE_KEY_EMAIL => JSON.parse(localStorage.getItem(setFormDataFromLS));
+const setFormDataFromLS = e => {
     formData[e.target.name] = e.target.value;
-});
+    localStorage.setItem(STORAGE_KEY_EMAIL, JSON.stringify(formData));
+}
+
+// const formEl = document.querySelector('.feedback-form');
+// const inputEl = document.querySelector('input[name="email"]');
+// const textareaEl = document.querySelector('textarea[name="message"]');
+
+// formEl.addEventListener('submit', onFormSubmit);
+// formEl.addEventListener('input', throttle(onDataToForm, 500));
+
+// // const throttle = require('lodash.throttle'); //викликаю throttle → частина бібліотеки Lodash
+
+// saveDataTextarea();
+
+// function onDataToForm(e) {
+//   const emailValue = e.currentTarget.elements.email.value;
+//   const msgValue = e.currentTarget.elements.message.value;
+
+//   const arrayEntryData = { email: emailValue, message: msgValue };
+
+//   localStorage.setItem('feedback-form-state', JSON.stringify(arrayEntryData)); //записую масив введених даних в localStorage
+// }
+
+// function saveDataTextarea() {
+//   const saveTextarea = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+//   if (saveTextarea) {
+//     inputEl.value = saveTextarea.email || ''; // Можливо ці умови слід поєднати!
+//   }
+//   if (saveTextarea) {
+//     textareaEl.value = saveTextarea.message || '';
+//   }
+// }
+
+// function onFormSubmit(e) {
+//   e.preventDefault();
+
+//   const dataLocalStorage = JSON.parse(
+//     localStorage.getItem('feedback-form-state')
+//   );
+
+//   if (dataLocalStorage) {
+//     console.log('Введені дані: ', dataLocalStorage); // Вивожу в консоль масив введених даних
+//   }
+
+//   localStorage.removeItem('feedback-form-state'); //Видаляю дані з localStorage
+
+//   e.currentTarget.reset(); //Очищаю поля форми
+// }
+
+
+// const savedEmail = localStorage.getItem('formData');
+// const parsedEmail = JSON.parse(savedEmail);
 
 populateMessageOutput();
 function onSubmitForm(evt) {
