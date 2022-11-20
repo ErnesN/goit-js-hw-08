36cdaf5db99dc89642a164e3c635ac9b867cc26c
Ernes Nafieiev <demodekoz@gmail.com>
2022-11-21 00:41:10 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,43 +1,20 @@
 // Add imports above this line
-// import { galleryItems } from './gallery-items';
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
-const galleryEl = document.querySelector(".gallery");
+const galleryEl = document.querySelector('.gallery');
 
-const createGalleryItems = ({ preview, original, description }) => 
-  `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`;
+const createGalleryItems = ({ preview, original, description }) =>
+  `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
 
 const makeGalleryItem = galleryItems.map(createGalleryItems).join('');
 galleryEl.insertAdjacentHTML('beforeend', makeGalleryItem);
 
-
-const onGalleryImageClick = e => {
-  e.preventDefault();
-  if (e.code === 'Escape') {
-    modal.close();
-  }
-
-  const createMarkupModalElementsGallery = basicLightbox.create(`
-    <img src="${e.target.dataset.source}" width="auto" height="auto">`);
-  createMarkupModalElementsGallery.show();
-
-  document.addEventListener('keydown', e => {
-    if (e.code === 'Escape') {
-      createMarkupModalElementsGallery.close();
-    }
-  });
-};
-
-galleryEl.addEventListener('click', onGalleryImageClick);
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
